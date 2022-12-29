f1077a39890cf0f29abde24cdc5008d8575cc83d
jarwisai <98968495+jarwisai@users.noreply.github.com>
2022-12-29 14:36:15 +0530

    Update stuff.py

--- helper/stuff.py
+++ helper/stuff.py
@@ -33,8 +33,8 @@ async def start(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/CompressorBot"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("UPDATES", url="telegram.me/OmegaBots"),
+                Button.url("DEVELOPER", url="https://t.me/ZOK4574"),
             ],
         ],
     )
@@ -60,8 +60,8 @@ async def beck(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("UPDATES", url="telegram.me/OmegaBots"),
+                Button.url("DEVELOPER", url="https://t.me/ZOK4574"),
             ],
         ],
     )
